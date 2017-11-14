1edd8c31536897994fbb47c1b08494388e8d477b
Heiko Braun <ike.braun@googlemail.com>
2017-11-14 11:51:57 +0100

    Fix licenses gen for secured booster

--- generate-licenses.sh
+++ generate-licenses.sh
@@ -6,7 +6,7 @@ set -euxo pipefail
 : ${BOOSTER_HOME?"Need the location of the boosters"}
 
 function main() {
-  for booster in "wfswarm-rest-http" "wfswarm-configmap" "wfswarm-health-check" "wfswarm-rest-http-crud"
+  for booster in "wfswarm-rest-http" "wfswarm-rest-http-secured" "wfswarm-configmap" "wfswarm-health-check" "wfswarm-rest-http-crud"
   do
 
     dir="$BOOSTER_HOME/$booster"
@@ -21,6 +21,34 @@ function main() {
 
     mvn clean package \
     -Dbooster.pom.file="$dir/pom.xml" \
+    -Dbooster.name="$name" \
+    -Dbooster.assembly.name="$name" \
+    -Dbooster.product.build="$name" \
+    -Dbooster.version="$version"
+
+    cp -R "target/license-project/target/licenses" "$dir/src/"
+
+    echo "Done generating licenses for $dir"
+
+  done
+}
+
+function custom() {
+  for booster in $1
+  do
+
+    dir="$BOOSTER_HOME/$booster"
+    name=`basename $dir`
+    version=`mvn -f $dir/pom.xml -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec`
+
+    # preflight
+    if [ -f "$dir/src/licenses/licenses.html" ]; then
+       echo "ERROR: Licenses already present, exiting..."
+       exit 1
+    fi
+
+    mvn clean package \
+    -Dbooster.project.dir="$dir" \
     -Dbooster.name="$name" \
     -Dbooster.assembly.name="$name" \
     -Dbooster.product.build="$name" \
@@ -64,40 +92,20 @@ function cb() {
   done
 }
 
-# sso
-function sso() {
-  for module in "app"
-  do
 
-    dir="$BOOSTER_HOME/wfswarm-rest-http-secured"
-    base=`basename $dir`
-    name="$base-$module"
-    version=`mvn -f $dir/pom.xml -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec`
 
-    # preflight
+if [ $# -eq 0 ]; then
+    echo "Processing all boosters"
+    cd $GENERATOR_HOME
+    main
+    cb
+else
 
-    if [ -f "$dir/$module/src/licenses/licenses.html" ]; then
-       echo "ERROR: Licenses already present, exiting..."
-       exit 1
+    if [ ! -d "$1" ]; then
+      echo "Directory does not exist: $1"
+      exit 1
     fi
 
-    mvn clean package \
-    -Dbooster.pom.file="$dir/$module/pom.xml" \
-    -Dbooster.name="$name" \
-    -Dbooster.assembly.name="$name" \
-    -Dbooster.product.build="$name" \
-    -Dbooster.version="$version" \
-    -Dbooster.project.dir="$dir/$module"
-
-    cp -R "target/license-project/target/licenses" "$dir/$module/src/"
-
-    echo "Done generating licenses for $dir/$module"
-  done
-}
-
-# exec
-cd $GENERATOR_HOME
-
-main
-cb
-sso
+    cd $GENERATOR_HOME
+    custom $1
+fi
