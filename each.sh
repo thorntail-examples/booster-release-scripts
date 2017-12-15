08a5c431e9fc65a2c21734add632cb718710f098
Heiko Braun <ike.braun@googlemail.com>
2017-12-15 08:40:05 +0100

    Allow for each.sh to consider subdirectories (prod booster)

--- each.sh
+++ each.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-find . -maxdepth 1 -type d -not -name '.*' -exec bash -c "echo '{}' && cd '{}' && $1" \;
+case "$1" in
+  "-p")
+    target="prod"
+    ;;
+  "-c")
+    target="."
+    ;;
+  *)
+    echo "Usage: each.sh <-p|-c>"
+    exit 1
+    ;;
+esac
+
+find $target -maxdepth 1 -type d -not -name '.*' -exec bash -c "echo '{}' && cd '{}' && $2" \;
