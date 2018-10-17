ebe9bb86b87d83b4c421b46e28a58aa09974eb07
Ladislav Thon <lthon@redhat.com>
2018-10-17 17:20:24 +0200

    improve the delimiter that each.sh prints for each processed directory

--- each.sh
+++ each.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+heading=$(printf '=%.0s' {1..30})
+
 case "$1" in
   "-p")
-    find . -maxdepth 1 -type d -not -name '.*' -name '*-redhat' -exec bash -c "echo '{}' && cd '{}' && $2" \;
+    find . -maxdepth 1 -type d -not -name '.*' -name '*-redhat' -exec bash -c "echo -e '\033[1m$heading {} $heading\033[0m' && cd '{}' && $2" \;
     ;;
   "-c")
-    find . -maxdepth 1 -type d -not -name '.*' -not -name '*-redhat' -exec bash -c "echo '{}' && cd '{}' && $2" \;
+    find . -maxdepth 1 -type d -not -name '.*' -not -name '*-redhat' -exec bash -c "echo -e '\033[1m$heading {} $heading\033[0m' && cd '{}' && $2" \;
     ;;
   *)
     echo "usage: each.sh <-p|-c> <command>"
