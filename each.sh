f69611c6c413c54507901315dad6eb54180625dc
Ladislav Thon <lthon@redhat.com>
2019-04-26 18:14:02 +0200

    few little improvements to the booster release scripts

--- each.sh
+++ each.sh
@@ -4,10 +4,10 @@ heading=$(printf '=%.0s' {1..30})
 
 case "$1" in
   "-p")
-    find . -maxdepth 1 -type d -not -name '.*' -name '*-redhat' -exec bash -c "echo -e '\033[1m$heading {} $heading\033[0m' && cd '{}' && $2" \;
+    find . -maxdepth 1 -type d -not -name '.*' -name '*-redhat' -exec bash -c "echo -e '\033[32m$heading {} $heading\033[0m' && cd '{}' && $2" \;
     ;;
   "-c")
-    find . -maxdepth 1 -type d -not -name '.*' -not -name '*-redhat' -exec bash -c "echo -e '\033[1m$heading {} $heading\033[0m' && cd '{}' && $2" \;
+    find . -maxdepth 1 -type d -not -name '.*' -not -name '*-redhat' -exec bash -c "echo -e '\033[32m$heading {} $heading\033[0m' && cd '{}' && $2" \;
     ;;
   *)
     echo "usage: each.sh <-p|-c> <command>"
